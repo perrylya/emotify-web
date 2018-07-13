b3ca662c7214f08a6c127c3a59b6ae66ed2455e8
perrylya <perrylya@gmail.com>
2018-07-13 15:06:30 -0700

    ternary

--- frontend/components/photopage.js
+++ frontend/components/photopage.js
@@ -2,37 +2,60 @@ import React from 'react';
 import Webcam from 'react-webcam';
 
 class WebcamCapture extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      image: ''
+    }
+  }
   setRef(webcam) {
     this.webcam = webcam;
   }
 
   capture() {
     const imageSrc = this.webcam.getScreenshot();
+      console.log(imageSrc)
+    this.setState({image: imageSrc})
   };
+
+  takeAnotherPhoto() {
+    this.setState({image: ''})
+  }
 
   render() {
     const videoConstraints = {
-      width: 720,
-      height: 720,
-      borderRadius: 720/2,
+      width: 300,
+      height: 300,
+      borderRadius: 100,
       borderWidth: 2,
       borderColor: 'black',
       facingMode: 'user',
     };
 
-      return (
+    return (
       <div>
-        <Webcam
-          audio={false}
-          height={350}
-          ref={this.setRef}
-          screenshotFormat="image/jpeg"
-          width={350}
-          videoConstraints={videoConstraints}
-        />
-        <div>
-          <button onClick={this.capture}>Capture photo</button>
-        </div>
+      {this.state.image ?
+          <div>
+            <img src={this.state.image}/>
+            <div>
+              <button onClick={this.takeAnotherPhoto.bind(this)}>Retake photo</button>
+            </div>
+          </div>
+        :
+          <div>
+            <Webcam
+              audio={false}
+              height={350}
+              ref={this.setRef.bind(this)}
+              screenshotFormat="image/jpeg"
+              width={350}
+              style={videoConstraints}
+            />
+            <div>
+              <button onClick={this.capture.bind(this)}>Capture photo</button>
+            </div>
+          </div>
+        }
       </div>
     );
   }
