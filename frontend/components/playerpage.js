89cc3edcf3779ead2aff2ba58a116c779a7c3041
git checkout -b sisqrd <sisiyu.design@gmail.com>
2018-07-14 01:13:33 -0700

    wew

--- frontend/components/playerpage.js
+++ frontend/components/playerpage.js
@@ -14,10 +14,7 @@ class Playerpage extends React.Component {
   }
 
   render() {
-    var inner = normal;
-    if(this.state.hover) {
-        inner = hover;
-    }
+ 
     return (
     
       <div className="playerpage"  > 
@@ -25,7 +22,7 @@ class Playerpage extends React.Component {
            <div className="cart" style={{background: `linear-gradient(to bottom, #FFB6C1, 	#F0E68C` }}>
            <Animations2/> 
             <div id="sidebar"> 
-                <div > 
+                <div> 
                   <div> Artist Name:  </div> <br/> 
              
                   <div>  Song Name: </div> 
@@ -38,33 +35,6 @@ class Playerpage extends React.Component {
   }
 }
 
-var outer = {
-  height: '120px',
-  width: '200px',
-  margin: '100px',
-  backgroundColor: 'green',
-  cursor: 'pointer',
-  position: 'relative'
-}
 
-var normal = {
-  position: 'absolute',
-  top: 0,
-  bottom: 0,
-  left: 0,
-  right: 0,
-  backgroundColor: 'red',
-  opacity: 0
-}
-
-var hover = {
-  position: 'absolute',
-  top: 0,
-  bottom: 0,
-  left: 0,
-  right: 0,
-  backgroundColor: 'red',
-  opacity: 1
-}
 
 export default Playerpage; 
