a8615382a69098ad4fc932821a45c03572e34f5e
perrylya <perrylya@gmail.com>
2018-07-13 17:29:42 -0700

    latest

---
+++ frontend/components/playerpage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Visualizer extends React.Component {
+  render() {
+    return ()
+  }
+}
+
+//when mouse moves
+export default class MusicPlayer extends React.Component {
+  render() {
+    return ()
+  }
+}
