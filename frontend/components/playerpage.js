604521b044df4cae394852ed4118017298d61afc
perrylya <perrylya@gmail.com>
2018-07-14 03:22:10 -0700

    playerpage

--- frontend/components/playerpage.js
+++ frontend/components/playerpage.js
@@ -14,29 +14,16 @@ class Playerpage extends React.Component {
   }
 
   render() {
- 
+
     return (
-
-      <div className="playerpage"  >
-
-           <div className="cart" style={{background: `linear-gradient(to bottom, #FFB6C1, 	#F0E68C` }}>
-           <Animations2/> 
-            <div id="sidebar"> 
-  
-                <div className="musicbar" > 
-                  <h2> Artist Name: Pharrel Williams  </h2> 
-                  <h2>  Song Name: Happy </h2> 
-                </div> 
-                <div>
-                <Button.Group>
-                  <Button labelPosition='left' icon='left chevron' content='Back' />
-                  <Button icon='stop' content='Stop' />
-                  <Button labelPosition='right' icon='right chevron' content='Forward' />
-                </Button.Group>
-                 </div>
-            </div>
-        </div> 
-    </div> 
+      <div className="playerpage" style={{background: `linear-gradient(to bottom, #FFB6C1, 	#F0E68C` }}>
+           <div className="cart" >
+           <Animations2/>
+              <div id="sidebar">
+                <iframe src="https://open.spotify.com/embed?uri=spotify:album:1DFixLWuPkv3KT3TnV35m3" width="100%" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+              </div>
+        </div>
+    </div>
 
     )
   }
